5a7c6459095c9b6609a0ae5f3eaa2415932797c4
Ashley Nolan <nolly00@gmail.com>
2014-06-02 10:40:22 +0100

    Updating config to hide app keys

--- config/config.js
+++ config/config.js
@@ -18,42 +18,27 @@ module.exports = {
 		app: {
 			name: 'Twitter vote counter - local'
 		},
-		twitter: {
-			consumer_key: 'aFAe6IA7vIAbt8BVo8CQ',
-			consumer_secret: 'lwQw74B6luoLabYoDudX4M97H57LiAQCYnSzHOWdI1g',
-			access_token_key: '72845380-oKSjRnmd1J9DrYFOLLDUbbaxqtm6KvFfhGCPni2Uo',
-			access_token_secret: 'dLmIIjVXvwMcyu5H2dVKq7xkvgzvJ08GHh8as4bK08'
-		},
+		twitter: require('./privconfig-twitter')['local'],
 		global: sharedConfig
 	},
 
-	development: {
+	dev: {
 		mode: 'development',
 		port: 3002,
 		app: {
 			name: 'Twitter vote counter - Dev'
 		},
-		twitter: {
-			consumer_key: '',
-			consumer_secret: '',
-			access_token_key: '',
-			access_token_secret: ''
-		},
+		twitter: require('./privconfig-twitter')['dev'],
 		global: sharedConfig
 	},
 
-	production: {
+	prod: {
 		mode: 'production',
 		port: 3002,
 		app: {
 			name: 'Twitter vote counter - Prod'
 		},
-		twitter: {
-			consumer_key: '',
-			consumer_secret: '',
-			access_token_key: '',
-			access_token_secret: ''
-		},
+		twitter: require('./privconfig-twitter')['prod'],
 		global: sharedConfig
 
 	},
